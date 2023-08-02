76f77b20f4919417871a75efbcaa9c71b4136a79
Tim <timofeyobraztsov@gmail.com>
2023-08-02 10:37:26 -0700

    generated linebreaks

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,31 @@
-use pathfinding::prelude::astar;
+// use pathfinding::prelude::astar;
 use std::fs;
+const LINEBREAK: u32 = 25;
+// #[derive(Clone, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
+// struct Pos(i32, i32);
+// impl Pos {
+//     fn distance(&self, other: &Pos) -> u32 {
+//         (self.0.abs_diff(other.0) + self.1.abs_diff(other.1))
+//     }
 
-#[derive(Clone, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
-struct Pos(i32, i32);
-impl Pos {
-    fn distance(&self, other: &Pos) -> u32 {
-        (self.0.abs_diff(other.0) + self.1.abs_diff(other.1))
-    }
+//     fn successors(&self) -> Vec<(Pos, u32)> {
+//         let &Pos(x, y) = self;
 
-    fn successors(&self) -> Vec<(Pos, u32)> {
-        let &Pos(x, y) = self;
-
-        vec![
-            Pos(x + 1, y + 2),
-            Pos(x + 1, y - 2),
-            Pos(x - 1, y + 2),
-            Pos(x - 1, y - 2),
-            Pos(x + 2, y + 1),
-            Pos(x + 2, y - 1),
-            Pos(x - 2, y + 1),
-            Pos(x - 2, y - 1),
-        ]
-        .into_iter()
-        .map(|p| (p, 1))
-        .collect()
-    }
-}
+//         vec![
+//             Pos(x + 1, y + 2),
+//             Pos(x + 1, y - 2),
+//             Pos(x - 1, y + 2),
+//             Pos(x - 1, y - 2),
+//             Pos(x + 2, y + 1),
+//             Pos(x + 2, y - 1),
+//             Pos(x - 2, y + 1),
+//             Pos(x - 2, y - 1),
+//         ]
+//         .into_iter()
+//         .map(|p| (p, 1))
+//         .collect()
+//     }
+// }
 fn main() {
     let contents = fs::read_to_string("./README2.txt").expect("failed to read");
     let mut numbers = contents.split(' ').map(|num| num.parse::<u32>());
@@ -33,6 +33,7 @@ fn main() {
     let mut endstring = String::from("");
     let mut line: Vec<u32> = vec![];
     let mut index = 1;
+
     let mut nextnum = numbers.next().unwrap();
     while index < 9765625 {
         // println!("{index} ,{:?}", &nextnum);
@@ -48,7 +49,7 @@ fn main() {
             endstring += " ";
             // line.push(1);
         }
-        if index % 1953125 == 0 {
+        if index % LINEBREAK == 0 {
             endstring += "\r\n";
             // numcollection.push(line.clone());
             // line.clear();
@@ -57,11 +58,11 @@ fn main() {
     }
     endstring += "1";
     println!("writing");
-    fs::write("numcollection.txt", endstring).expect("failed to write to file");
+    fs::write(format!("{LINEBREAK}.txt"), endstring).expect("failed to write to file");
 
     // fs::write("arrays.json", format!("{:?}", numcollection)).expect("failed to write to file");
     // println!("{:?}", &numcollection);
-    const GOAL: Pos = Pos(1953125, 3125);
+    // const GOAL: Pos = Pos(1953125, 3125);
     // let result = astar(
     //     &Pos(1, 1),
     //     |p| p.successors(),
